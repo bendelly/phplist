208c461394cb80ff5c0af3214e17392fd3f30fb8
diszbohoc <44508631+diszbohoc@users.noreply.github.com>
2021-07-07 20:36:56 +0200

    init

---
+++ list.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu-HU">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" initial-scale=1"/>
+	<link type="text/css" rel="stylesheet" href="reset.css">
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <title>Page Title</title>
+</head>
+<body>
+	<form method="post">
+		<label>Lista szövege</label>
+		<input type="text" name="szoveg">
+		<label>Lista elemszáma</label>
+		<input type="text" name="elemszam">
+		<input type="submit" value="Lista kiíratása">
+	</form>
+    
+</body>
+</html>
